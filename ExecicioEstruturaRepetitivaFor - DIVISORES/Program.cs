7b8b3d704b8dce293e7024ffd00ee9e768d9d9a6
Eduardo Kropniczki <dudukrop@gmail.com>
2024-04-19 18:41:12 -0300

    first commit

---
+++ ExecicioEstruturaRepetitivaFor - DIVISORES/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio {
+    class Program {
+        static void Main(string[] args) {
+
+            Console.WriteLine("Digite um número:");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++) {
+                if (n % i == 0)
+                    Console.WriteLine(i);
+            }
+            Console.ReadLine();
+        }
+    }
+}
